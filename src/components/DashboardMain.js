1c2d1762e7e0ea5db53df9e1206add5b11eab6af
wiseman-umanah <wisemanumanah@gmail.com>
2025-03-02 20:25:34 +0100

    Added new changes to the dashboard

--- src/components/DashboardMain.js
+++ src/components/DashboardMain.js
@@ -1,26 +1,62 @@
 import React from 'react';
+import { BiSolidCalendarEvent } from "react-icons/bi";
+import { TiUserOutline } from "react-icons/ti";
+import { IoShareSocialOutline } from "react-icons/io5";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { MdMoreHoriz } from "react-icons/md";
 
 const DashboardMain = () => {
 	return (
-		<section className="w-full h-full border border-blue-900 flex flex-col">
-			<aside className="w-full h-12 border border-red-900">
-				top
-			</aside>
-			<div className="card-container flex border border-purple-900 h-full">
-				<div className="card-tile w-full border border-blue-900 flex flex-col gap-2 p-5">
-					<div className="analytics-1 w-full h-48 bg-blue-900 rounded-lg"></div>
-					<div className="analytics-2 flex gap-2">
-						<div className="small-div w-1/2 h-48 bg-blue-900 rounded-lg"></div>
-						<div className="small-div w-1/2 h-48 bg-blue-900 rounded-lg"></div>
+		<section className="bg-gray-200 flex flex-col p-2.5 w-[calc(100%-250px)] h-full">
+			<aside className="flex justify-between items-center h-12 border">
+				<div>My Dashboard</div>
+				<div className="flex">
+					<div className="flex gap-2.5">
+						<div className="flex items-center gap-2.5 px-2.5 bg-white rounded border border-gray-400">
+							<BiSolidCalendarEvent />
+							<span>Period</span>
+							<IoMdArrowDropdown />
+						</div>
+						<div className="flex items-center gap-2.5 px-2.5 bg-white rounded border border-gray-400">
+							<TiUserOutline />
+							<span>User</span>
+							<IoMdArrowDropdown  />
+						</div>
+					</div>
+					<hr className="border-gray-400 mx-2.5" />
+					<div className="flex gap-2.5">
+						<div className="flex items-center gap-2.5 px-2.5 bg-white rounded border border-gray-400">
+							<IoShareSocialOutline  />
+							<span>Share</span>
+						</div>
+						<div className="flex items-center gap-2.5 px-2.5 bg-white rounded border border-gray-400">
+							<span>Export</span>
+							<IoMdArrowDropdown  />
+						</div>
+						<div className="flex items-center gap-2.5 px-2.5 bg-white rounded border border-gray-400">
+							<MdMoreHoriz />
+						</div>
 					</div>
 				</div>
-				<div className="card-tile w-full border border-blue-900 flex flex-col gap-2 p-5">
-					<div className="analytics-1 w-full h-48 bg-blue-900 rounded-lg"></div>
-					<div className="analytics-2 flex gap-2">
-						<div className="small-div w-1/2 h-48 bg-blue-900 rounded-lg"></div>
-						<div className="small-div w-1/2 h-48 bg-blue-900 rounded-lg"></div>
+			</aside>
+			<div className="flex gap-2.5 h-full mt-6">
+				{/* Card Tile 1 */}
+				<div className="card-tile flex flex-col gap-2.5 w-full border">
+					<div className=" w-full h-48 bg-gray-50 "></div>
+					<div className=" flex gap-2.5">
+						<div className="small-div w-1/2 h-48 bg-gray-50 "></div>
+						<div className="small-div w-1/2 h-48 bg-gray-50 "></div>
 					</div>
-					<div className="analytics-1 w-full h-48 bg-blue-900 rounded-lg"></div>
+				</div>
+
+				{/* Card Tile 2 */}
+				<div className="flex flex-col gap-2.5 w-full border ">
+					<div className=" w-full h-48 bg-gray-50 "></div>
+					<div className=" flex gap-2.5">
+						<div className="small-div w-1/2 h-48 bg-gray-50 "></div>
+						<div className="small-div w-1/2 h-48 bg-gray-50 "></div>
+					</div>
+					<div className=" w-full h-48 bg-gray-50"></div>
 				</div>
 			</div>
 		</section>
