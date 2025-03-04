1957f24de7b2fd6ed65434d267ad121e39d740bf
wiseman-umanah <wisemanumanah@gmail.com>
2025-03-04 09:50:08 +0100

    Testing google chart

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-import MinNav from './components/MinNav';
-import DashboardMain from './pages/Dashboards/UserDashboards/DashboardMain';
-import DealsConversion from './pages/Reports/UserReports/DealsConversion';
 import { PiGraduationCapLight } from "react-icons/pi";
 
 
@@ -14,13 +11,10 @@ const App = () => {
 			<div className="h-screen overflow-hidden relative">
 				<Header />
 				<Sidebar />
-				<main className="flex mt-14 ml-12 h-full">
-					<MinNav />
-					<Routes>
-						<Route path="/my-dashboard" element={<DashboardMain />} />
-						<Route path="/reports/my-reports/deals-conversion" element={<DealsConversion />} />
-					</Routes>
-				</main>
+				<Routes>
+					<Route path="/my-dashboard" element={<DashboardMain />} />
+					<Route path="/reports/my-reports/deals-conversion" element={<DealsConversion />} />
+				</Routes>
 				<div className='absolute bottom-5 right-5 p-4 bg-purple-900 text-white rounded-full'>
 					<PiGraduationCapLight className='icon-style'/>
 				</div>
